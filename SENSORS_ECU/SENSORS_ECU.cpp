96e773f43568a5865d58b66688c14cf9d5876add
sironark <gabriel_lopeslomeu@hotmail.com>
2023-09-06 16:14:52 -0300

    versão com cpp e .oil do sender Fault signal #6

--- SENSORS_ECU/SENSORS_ECU.cpp
+++ SENSORS_ECU/SENSORS_ECU.cpp
@@ -90,8 +90,7 @@ TASK(SenderRainSensor){
 		 Serial.print("CAN Rain SENT.");
 		//Could also print the value that was sent.
 	}
-	TerminateTask();
-	  
+	TerminateTask();  
 }
 //---------------------------------Send CAN Break msg.
 TASK(SenderBreakSensor){
@@ -117,6 +116,7 @@ TASK(SenderBreakSensor){
 }
 //---------------------------------Send CAN Gas msg.
 TASK(SenderGasSensor){
+
 	if(Gas_pedal_sensor){
 		Gas_pedal_base = 1;
 	}else{
