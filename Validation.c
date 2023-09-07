d7edcfaf7b8e30412894d787d101bb0dc4b6d70c
sironark <gabriel_lopeslomeu@hotmail.com>
2023-09-07 16:07:21 -0300

    feat: check Safe Distance in the rain and Relative Distance to Safe Distance

--- Validation.c
+++ Validation.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-float checkCollision(int ACC_enable, double relativeDistance) {
+float checkCollision(int ACC_enable, float relativeDistance) {
     if(ACC_enable == 1 && relativeDistance <= 0){
         printf("Collision detected. Take the necessary actions...\n");
         return 1;
@@ -32,17 +32,41 @@ float checkValidationSensors(short int ACC_enable, int Fault_signal, int Gas_ped
     }
 }
 
+float checkRainSafeDistance(int rain_signal, int D_distance, int V_ego, float safeDistance){
+    if(rain_signal){
+        if(safeDistance != (D_distance + V_ego*6)){
+            printf("Safe distance is not correct for a wet street\n");
+            return 1;
+        }
+    }
+    return 0;
+}
+
+float checkRelativeSafeDistance(float safeDistance, float relativeDistance){
+    if(relativeDistance < safeDistance){
+        printf("The vehicle is closer than the limit\n");
+        return 1;
+    }
+}
+
+
+//---------------------- simulating calling the check functions------------------
 int main() {
     int ACC_enable = 1;
-    double relativeDistance = -5.0;
+    float relativeDistance = 0;
     int Fault_signal = 1; 
     int Gas_pedal = 1; 
-    int Brake_pedal = 1; 
+    int Brake_pedal = 1;
+
     int rain_signal = 1;
+    int D_distance = 10;
+    int V_ego = 17;
+    float safeDistance = 112; //distance for a wet street 
 
     checkCollision(ACC_enable, relativeDistance);
-    checkValidationSensors(ACC_enable, Fault_signal, Gas_pedal, Brake_pedal)
-
+    checkValidationSensors(ACC_enable, Fault_signal, Gas_pedal, Brake_pedal);
+    checkRainSafeDistance(rain_signal, D_distance, V_ego, safeDistance);
+    checkRelativeSafeDistance(safeDistance, relativeDistance);
     return 0;
 }
 
