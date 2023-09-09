31da47b68e8d86c7997dd8e501a9a5b82c3ce877
vitorfccarvalho <vitorfassanaro@gmail.com>
2023-09-09 19:48:08 -0300

    More changes about the Simulation.

--- Simu_Arduino.ino
+++ Simu_Arduino.ino
@@ -5,6 +5,7 @@
 float Ego_pos, Lead_pos;
 float Ego_velo, Lead_velo;
 float Ego_ace = 0, Lead_ace = 0;
+float Relative_velo = 0;
 
 Lead_pos = 50.0;  // Initial lead car position (m)
 Ego_pos  = 10.0;  // Initial ego car position  (m)
@@ -19,17 +20,17 @@ float interval = 0.001;
 int counter = 0;
 //--------------------------------Can Setup--------------------------------/
 MCP_CAN CAN1(10);
-
+//Ego Speed
 #define mSim1_ID			0xEC100007
 #define mSim1_DLC			8
-
+//Relative Distance
 #define mSim2_ID			0xEC100008
 #define mSim2_DLC			8
-
+//Ego Acceleration
 #define mSim3_ID			0xACC00001
 #define mSim3_DLC			8
-
-#define mSim4_ID			0xACC00002
+//Relative Speed
+#define mSim4_ID			0xEC100009
 #define mSim4_DLC			8
 
 #define CAN_EXTID           1
@@ -68,6 +69,7 @@ Lead_pos += (Lead_velo*interval);
 counter++;
 
 Relative_distance_pres = Lead_pos - Ego_pos;
+Relative_velo = Lead_velo - Ego_velo;
 
 if(Relative_distance > 200){
     Relative_distance_pres = 200;
@@ -75,24 +77,28 @@ if(Relative_distance > 200){
     Relative_distance_pres = Relative_distance_pres;
 }
 
-static byte ret = 0;
-
 CAN1.readMsgBuf(&mID, &mDLC, mDATA);
 
 if((mID & mSim3_ID) == mSim3_ID){
-    Ego_ace = mDATA[];
+    Ego_ace = mDATA[4];
     }else{
         Ego_ace = Ego_ace;
     }
 
+//Relative Distance CAN Message Sender
+mSim2_data[4] = Relative_distance_pres;
 CAN1.sendMsgBuf(mSim2_ID,CAN_EXTID,mSim2_DLC,mSim2_data);
 
 delay(20);
 
+//Ego Speed CAN Message Sender
+mSim1_data[4] = Ego_velo;
 CAN1.sendMsgBuf(mSim1_ID,CAN_EXTID,mSim1_DLC,mSim1_data);
 
 delay(20);
 
+//Relative Speed CAN Message Sender
+mSim4_data[4] = Relative_velo;
 CAN1.sendMsgBuf(mSim4_ID,CAN_EXTID,mSim4_DLC,mSim4_data);
 
 while (counter < 50000);
